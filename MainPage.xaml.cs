fc1b5e40a4be02bbf87d90ceff5ce5ffb5cecdd7
Ronan-Francis <ronananthonyfrancis@gmail.com>
2023-05-26 20:06:45 +0100

    draw cube

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,24 +1,61 @@
-﻿namespace Tetris;
+﻿/*
+ */
+
+
+namespace Tetris;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+    int maxRow = 20;
+    int maxCol = 10;
+    int[,] _gameArray = new int[20, 10];
 
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+    public MainPage()
+    {
+        InitializeComponent();
+        InitializeGame();
+        drawShape(0, 0, 0);
+    }
+    private void InitializeGame()
+    {
+        gameGrid.HeightRequest = maxRow * 20;
+        gameGrid.WidthRequest = maxCol * 20;
+        for (int i = 0; i < maxRow; i++)
+        {
+            gameGrid.AddRowDefinition(new RowDefinition());
+        }
+        for (int i = 0; i < maxCol; i++)
+        {
+            gameGrid.ColumnDefinitions.Add(new ColumnDefinition());
+        }
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+        for (int r = 0; r < maxRow; r++)
+        {
+            for (int c = 0; c < maxCol; c++)
+            {
+                Image image = new Image();
+                image.Source = "blankcube.png";
+                image.HorizontalOptions = LayoutOptions.Center;
+                image.VerticalOptions = LayoutOptions.Center;
+                image.SetValue(Grid.RowProperty, r);
+                image.SetValue(Grid.ColumnProperty, c);
+                gameGrid.Children.Add(image);
+                //_gameArray[r,c] = 0;//0 = no color
+            }// end for (c = 0)
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+        } // end for(r = 0)
+    }
+
+    private void drawShape(int row, int col, int shape)
+    {
+        foreach (Image element in gameGrid.Children)
+        {
+            if (gameGrid.GetColumn(element) == col && gameGrid.GetRow(element) == row)
+            {
+                element.Source = "redcube.png";
+            }
+        }
+    }
 }
 
